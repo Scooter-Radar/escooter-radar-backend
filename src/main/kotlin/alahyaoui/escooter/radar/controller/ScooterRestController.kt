46be349cb57c3237ba3674a7c5a1a820ae103841
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-15 19:58:07 +0200

    Update zone parameter from RequestParam to PathVariable

--- src/main/kotlin/alahyaoui/escooter/radar/controller/ScooterRestController.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/controller/ScooterRestController.kt
@@ -2,17 +2,20 @@ package alahyaoui.escooter.radar.controller
 
 import alahyaoui.escooter.radar.entity.Scooter
 import alahyaoui.escooter.radar.service.ScooterService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/scooter")
 class ScooterRestController(private val scooterService: ScooterService) {
 
-    @GetMapping("/")
-    fun getScooters(@RequestParam zone: String): MutableIterable<Scooter> {
+    @GetMapping("/{zone}")
+    fun getScooters(@PathVariable zone: String): MutableIterable<Scooter> {
         return scooterService.getAllScootersBy(zone)
     }
 }
