30b5f5eda9afdc0f43d61a9d3189d4543aa39f71
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-16 00:41:49 +0200

    Remove unnecessary lines and add function addCompany a function that adds company value to provider response

--- src/main/kotlin/alahyaoui/escooter/radar/service/ScooterService.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/service/ScooterService.kt
@@ -2,57 +2,60 @@ package alahyaoui.escooter.radar.service
 
 import alahyaoui.escooter.radar.entity.Scooter
 import alahyaoui.escooter.radar.repository.ScooterRepository
-import org.springframework.context.annotation.Bean
-import org.springframework.http.HttpEntity
+import alahyaoui.escooter.radar.util.ScooterProviderJson
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
 @Service
-class ScooterService(private val db: ScooterRepository, private val restTemplate: RestTemplate) {
+class ScooterService(private val db: ScooterRepository) {
+
+    private val restTemplate = RestTemplate()
+
     fun getAllScootersBy(zone: String): Iterable<Scooter> {
         val scooters = mutableListOf<Scooter>()
-        findLime(zone)?.let { scooters.addAll(it) }
-        findBird(zone)?.let { scooters.addAll(it) }
-        findPony(zone)?.let { scooters.addAll(it) }
+
+        try {
+            findPony(zone)?.let { scooters.addAll(it) }
+        } catch (ex: Exception) { }
+
+        try {
+            findLime(zone)?.let { scooters.addAll(it) }
+        } catch (ex: Exception) { }
+
+        try {
+            findBird(zone)?.let { scooters.addAll(it) }
+        } catch (ex: Exception) { }
+
         db.saveAll(scooters)
         return db.findAll()
     }
 
     private fun findLime(zone: String): Array<Scooter>? {
-        val headers = HttpHeaders()
-        headers.accept = listOf(MediaType.APPLICATION_JSON)
-        val entity = HttpEntity<String>(headers)
-
         val uri = "https://data.lime.bike/api/partners/v2/gbfs/${zone}/free_bike_status.json"
-        val response = restTemplate.exchange(uri, HttpMethod.GET, entity, Array<Scooter>::class.java)
-        return response.body
+        val response = restTemplate.getForEntity(uri, ScooterProviderJson::class.java)
+        addCompany(response.body?.data?.bikes!!, "Lime")
+        return response.body?.data?.bikes
     }
 
     private fun findBird(zone: String): Array<Scooter>? {
-        val headers = HttpHeaders()
-        headers.accept = listOf(MediaType.APPLICATION_JSON)
-        val entity = HttpEntity<String>(headers)
-
         val uri = "https://mds.bird.co/gbfs/v2/public/${zone}/free_bike_status.json"
-        val response = restTemplate.exchange(uri, HttpMethod.GET, entity, Array<Scooter>::class.java)
-        return response.body
+        val response = restTemplate.getForEntity(uri, ScooterProviderJson::class.java)
+        addCompany(response.body?.data?.bikes!!, "Bird")
+        return response.body?.data?.bikes
     }
 
     private fun findPony(zone: String): Array<Scooter>? {
-        val headers = HttpHeaders()
-        headers.accept = listOf(MediaType.APPLICATION_JSON)
-        val entity = HttpEntity<String>(headers)
-
         val uri = "https://gbfs.getapony.com/v1/${zone}/en/free_bike_status.json"
-        val response = restTemplate.exchange(uri, HttpMethod.GET, entity, Array<Scooter>::class.java)
-        return response.body
+        val response = restTemplate.getForEntity(uri, ScooterProviderJson::class.java)
+        addCompany(response.body?.data?.bikes!!, "Pony")
+        return response.body?.data?.bikes
     }
 
-    @Bean
-    fun restTemplate(): RestTemplate {
-        return RestTemplate()
+    private fun addCompany(scooters: Array<Scooter>, company: String) {
+        for (i in scooters.indices!!) {
+            scooters[i].company = company
+        }
     }
 }
