914976e4b25bdaee19f46f4de4faff1e64113e2f
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-15 18:48:41 +0200

    Fix Problems in ScooterService class

--- src/main/kotlin/alahyaoui/escooter/radar/service/ScooterService.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/service/ScooterService.kt
@@ -9,12 +9,11 @@ import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
-import java.util.Arrays
 
 @Service
 class ScooterService(private val db: ScooterRepository, private val restTemplate: RestTemplate) {
     fun getAllScootersBy(zone: String): MutableIterable<Scooter> {
-        val scooters: MutableList<Scooter> = mutableListOf<Scooter>()
+        val scooters = mutableListOf<Scooter>()
         findLime(zone)?.let { scooters.addAll(it) }
         findBird(zone)?.let { scooters.addAll(it) }
         findPony(zone)?.let { scooters.addAll(it) }
@@ -24,32 +23,32 @@ class ScooterService(private val db: ScooterRepository, private val restTemplate
 
     private fun findLime(zone: String): Array<Scooter>? {
         val headers = HttpHeaders()
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON))
-        val entity = HttpEntity<String>(headers);
+        headers.accept = listOf(MediaType.APPLICATION_JSON)
+        val entity = HttpEntity<String>(headers)
 
         val uri = "https://data.lime.bike/api/partners/v2/gbfs/${zone}/free_bike_status.json"
         val response = restTemplate.exchange(uri, HttpMethod.GET, entity, Array<Scooter>::class.java)
-        return response.getBody()
+        return response.body
     }
 
     private fun findBird(zone: String): Array<Scooter>? {
         val headers = HttpHeaders()
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON))
-        val entity = HttpEntity<String>(headers);
+        headers.accept = listOf(MediaType.APPLICATION_JSON)
+        val entity = HttpEntity<String>(headers)
 
         val uri = "https://mds.bird.co/gbfs/v2/public/${zone}/free_bike_status.json"
         val response = restTemplate.exchange(uri, HttpMethod.GET, entity, Array<Scooter>::class.java)
-        return response.getBody()
+        return response.body
     }
 
     private fun findPony(zone: String): Array<Scooter>? {
         val headers = HttpHeaders()
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON))
-        val entity = HttpEntity<String>(headers);
+        headers.accept = listOf(MediaType.APPLICATION_JSON)
+        val entity = HttpEntity<String>(headers)
 
         val uri = "https://gbfs.getapony.com/v1/${zone}/en/free_bike_status.json"
         val response = restTemplate.exchange(uri, HttpMethod.GET, entity, Array<Scooter>::class.java)
-        return response.getBody()
+        return response.body
     }
 
     @Bean
