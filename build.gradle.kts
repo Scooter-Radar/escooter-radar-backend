42566f8bdb558b69c6c31d9e8b35877709db6dc5
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-15 19:17:31 +0200

    Add Rest Controller Mock Test Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,13 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(module = "junit")
+		exclude(module = "mockito-core")
+	}
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 tasks.withType<KotlinCompile> {
