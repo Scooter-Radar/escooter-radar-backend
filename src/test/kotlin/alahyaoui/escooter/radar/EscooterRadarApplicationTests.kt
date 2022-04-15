3a77e2ffc4d7e9674389f9866aaeec3e5737fb67
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-15 18:44:21 +0200

    Spring Boot project initialization

---
+++ src/test/kotlin/alahyaoui/escooter/radar/EscooterRadarApplicationTests.kt
@@ -0,0 +1,13 @@
+package alahyaoui.escooter.radar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EscooterRadarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
