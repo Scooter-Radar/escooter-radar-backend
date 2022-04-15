28e4eb63fc72981d61118a7872d1c673fa15fb93
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-15 18:44:05 +0200

    Spring Boot project initialization

---
+++ src/main/kotlin/alahyaoui/escooter/radar/EscooterRadarApplication.kt
@@ -0,0 +1,11 @@
+package alahyaoui.escooter.radar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EscooterRadarApplication
+
+fun main(args: Array<String>) {
+	runApplication<EscooterRadarApplication>(*args)
+}
