6f1bddb051fa4b4b31c7441678b169b256b7baa1
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-18 14:18:02 +0200

    Make ScooterRepository inherit JpaRepository instead of CrudRepository

--- src/main/kotlin/alahyaoui/escooter/radar/repository/ScooterRepository.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/repository/ScooterRepository.kt
@@ -1,13 +1,13 @@
 package alahyaoui.escooter.radar.repository
 
 import alahyaoui.escooter.radar.entity.Scooter
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface ScooterRepository : CrudRepository<Scooter, UUID> {
+interface ScooterRepository : JpaRepository<Scooter, UUID> {
 
     @Query("SELECT s FROM Scooter s WHERE s.isDisabled = false AND s.isReserved = false")
     fun findAllAvailable(): Iterable<Scooter>
