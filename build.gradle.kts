be06f4b438633512087e33f327ef8c56c7ef2fae
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-21 19:48:01 +0200

    Add missing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,38 @@ repositories {
 }
 
 dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	// Tuto
+	//implementation("com.vividsolutions:jts-core:1.14.0")
+	implementation("com.bedatadriven:jackson-datatype-jts:2.4")
+	//implementation("org.postgresql:postgresql:42.1.4")
+	implementation("net.postgis:postgis-jdbc:2021.1.0")
+
+
+	// Hibernate Dependencies
+	//implementation("org.hibernate:hibernate-core")
+	implementation("org.hibernate:hibernate-spatial")
+
+	// Postgres Dependencies
+	//runtimeOnly("org.postgresql:postgresql")
+	//implementation("net.postgis:postgis-jdbc:2021.1.0")
+
+	// JTS Dependencies
+	implementation("org.locationtech.jts:jts-core:1.16.0")
+
+	// Jackson Dependencies
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	//implementation("org.n52.jackson:jackson-datatype-jts:1.2.10")
+
+	// Spring Boot dependencies
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
+
+	// Test Dependencies
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(module = "junit")
@@ -52,3 +76,14 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+// h2 Dependencies
+//runtimeOnly("com.h2database:h2")
+//implementation("org.hibernatespatial:hibernate-spatial-h2-geodb")
+
+// MariaDB Dependencies
+//implementation("mysql:mysql-connector-java")
+//implementation("ch.vorburger.mariaDB4j:mariaDB4j-springboot:2.2.3")
+
+// JTS Dependencies
+// implementation("org.locationtech.jts:jts-core:1.16.0")
