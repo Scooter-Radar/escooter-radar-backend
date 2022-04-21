9cd69fdde328023d062bb7b6601a1edd8f2351c0
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-21 19:49:22 +0200

    Try to make work hibernate spatial and postgis

--- src/main/kotlin/alahyaoui/escooter/radar/controller/ScooterRestController.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/controller/ScooterRestController.kt
@@ -3,10 +3,9 @@ package alahyaoui.escooter.radar.controller
 import alahyaoui.escooter.radar.entity.Scooter
 import alahyaoui.escooter.radar.service.ScooterService
 import alahyaoui.escooter.radar.util.toSlug
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.geo.GeoResults
+import org.springframework.data.geo.Point
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/scooter")
@@ -15,5 +14,15 @@ class ScooterRestController(private val scooterService: ScooterService) {
     @GetMapping("/{zone}")
     fun getScooters(@PathVariable zone: String): Iterable<Scooter> {
         return scooterService.getAllScootersBy(zone.toSlug())
+    }
+
+    @GetMapping("/location")
+    fun getScooters(@RequestParam latitude: Double, @RequestParam longitude: Double): Iterable<Scooter> {
+        return scooterService.findNear(longitude, latitude)
+    }
+
+    @GetMapping("/location2")
+    fun getScooters(@RequestParam latitude: Double, @RequestParam longitude: Double, @RequestParam distance: Double): Iterable<Scooter> {
+        return scooterService.findNear(longitude, latitude, distance)
     }
 }
