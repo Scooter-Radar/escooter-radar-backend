33e3fc55a84adf98a847b5ecd90b5c068f911690
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-04-23 00:48:37 +0200

    Rename methods and parameters

--- src/main/kotlin/alahyaoui/escooter/radar/controller/ScooterRestController.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/controller/ScooterRestController.kt
@@ -3,8 +3,6 @@ package alahyaoui.escooter.radar.controller
 import alahyaoui.escooter.radar.entity.Scooter
 import alahyaoui.escooter.radar.service.ScooterService
 import alahyaoui.escooter.radar.util.toSlug
-import org.springframework.data.geo.GeoResults
-import org.springframework.data.geo.Point
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,17 +10,12 @@ import org.springframework.web.bind.annotation.*
 class ScooterRestController(private val scooterService: ScooterService) {
 
     @GetMapping("/{zone}")
-    fun getScooters(@PathVariable zone: String): Iterable<Scooter> {
+    fun getScootersByZone(@PathVariable zone: String): Iterable<Scooter> {
         return scooterService.getAllScootersBy(zone.toSlug())
     }
 
     @GetMapping("/location")
-    fun getScooters(@RequestParam latitude: Double, @RequestParam longitude: Double): Iterable<Scooter> {
-        return scooterService.findNear(longitude, latitude)
-    }
-
-    @GetMapping("/location2")
-    fun getScooters(@RequestParam latitude: Double, @RequestParam longitude: Double, @RequestParam distance: Double): Iterable<Scooter> {
-        return scooterService.findNear(longitude, latitude, distance)
+    fun getScootersByLocationWithinDegree(@RequestParam latitude: Double, @RequestParam longitude: Double, @RequestParam degree: Double): Iterable<Scooter> {
+        return scooterService.findNear(latitude, longitude, degree)
     }
 }
