fcb9960885fb1eb669e78936036d3852bb98ae05
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-04-22 18:37:58 +0200

    Fix ScooterDto data class

--- src/main/kotlin/alahyaoui/escooter/radar/util/ScooterProvider.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/util/ScooterProvider.kt
@@ -8,10 +8,9 @@ import javax.persistence.Id
 
 data class ScooterProviderJson(val data: Data)
 
-data class Data(val bikes: Array<ScooterPonyDto>)
+data class Data(val bikes: Array<ScooterDto>)
 
-
-data class ScooterPonyDto(
+data class ScooterDto(
     @JsonProperty("bike_id")
     var bikeId: String,
 
@@ -21,30 +20,18 @@ data class ScooterPonyDto(
     @JsonProperty("lon")
     var longitude: Double,
 
-    @JsonProperty("is_disabled")
-    var isDisabled: Boolean,
-
     @JsonProperty("is_reserved")
     var isReserved: Boolean,
 
-    @JsonProperty("last_reported")
-    var lastReported: Long,
-
-    @JsonProperty("current_fuel_percent")
-    var currentFuelPercent: Double,
+    @JsonProperty("is_disabled")
+    var isDisabled: Boolean,
 
     @JsonProperty("current_range_meters")
     var currentRangeMeters: Double,
 
-    @JsonProperty("pricing_plan_id")
-    var pricingPlanId: String,
-
-    //@JsonProperty("rental_uris")
-    //var rentalUris: Array<String>,
-
-    @JsonProperty("system_id")
-    var systemId: String,
-
     @JsonProperty("vehicle_type_id")
-    var vehicleTypeId: String
+    var vehicleTypeId: String,
+
+    @JsonProperty("last_reported")
+    var lastReported: Long,
 )
