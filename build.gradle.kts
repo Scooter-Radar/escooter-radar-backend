11d910e280b853a857e3c94d2c321f537a36f61c
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-04-23 01:15:30 +0200

    Update dependencies to make postgis and hibernate spatial work

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,27 +27,18 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	// Tuto
-	//implementation("com.vividsolutions:jts-core:1.14.0")
-	implementation("com.bedatadriven:jackson-datatype-jts:2.4")
-	//implementation("org.postgresql:postgresql:42.1.4")
-	implementation("net.postgis:postgis-jdbc:2021.1.0")
-
-
 	// Hibernate Dependencies
-	//implementation("org.hibernate:hibernate-core")
 	implementation("org.hibernate:hibernate-spatial")
 
-	// Postgres Dependencies
-	//runtimeOnly("org.postgresql:postgresql")
-	//implementation("net.postgis:postgis-jdbc:2021.1.0")
+	// Postgis Dependencies
+	implementation("net.postgis:postgis-jdbc:2021.1.0")
 
 	// JTS Dependencies
 	implementation("org.locationtech.jts:jts-core:1.16.0")
 
 	// Jackson Dependencies
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	//implementation("org.n52.jackson:jackson-datatype-jts:1.2.10")
+	implementation("com.graphhopper.external:jackson-datatype-jts:0.10-2.5-1")
 
 	// Spring Boot dependencies
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -76,14 +67,3 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-
-// h2 Dependencies
-//runtimeOnly("com.h2database:h2")
-//implementation("org.hibernatespatial:hibernate-spatial-h2-geodb")
-
-// MariaDB Dependencies
-//implementation("mysql:mysql-connector-java")
-//implementation("ch.vorburger.mariaDB4j:mariaDB4j-springboot:2.2.3")
-
-// JTS Dependencies
-// implementation("org.locationtech.jts:jts-core:1.16.0")
