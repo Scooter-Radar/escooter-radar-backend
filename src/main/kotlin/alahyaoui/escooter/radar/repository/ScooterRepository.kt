132134ad32771faedb5f3b30731be92122d0630a
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-04-23 01:17:17 +0200

    Add findAvailableByLocationWithinDegree method allowing us to make search by location within a certain degree

--- src/main/kotlin/alahyaoui/escooter/radar/repository/ScooterRepository.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/repository/ScooterRepository.kt
@@ -1,15 +1,14 @@
 package alahyaoui.escooter.radar.repository
 
 import alahyaoui.escooter.radar.entity.Scooter
-//import alahyaoui.escooter.radar.entity.ScooterId
+import alahyaoui.escooter.radar.entity.ScooterId
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 
 @Repository
-interface ScooterRepository : JpaRepository<Scooter, /*ScooterId*/String> {
+interface ScooterRepository : JpaRepository<Scooter, ScooterId> {
 
     @Query("SELECT s FROM Scooter s WHERE s.isDisabled = false AND s.isReserved = false")
     fun findAllAvailable(): Iterable<Scooter>
@@ -18,30 +17,11 @@ interface ScooterRepository : JpaRepository<Scooter, /*ScooterId*/String> {
     fun findAllAvailableByZone(zone: String): Iterable<Scooter>
 
     @Query(
-        value = "SELECT s, ST_Distance(s.location,ST_SetSRID(ST_Point(:longitude,:latitude),4326)) AS distance "
-                + "FROM Scooter s "
-                + "ORDER BY s.location  <-> ST_SetSRID(ST_Point(:longitude,:latitude),4326) "
-                + "LIMIT 30",
+        value = "SELECT * " +
+                "FROM Scooter " +
+                "WHERE is_disabled = false AND is_reserved = false " +
+                "AND ST_DWithin(location, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326), :degree)",
         nativeQuery = true
     )
-    fun findAllByDistanceFromLocation(@Param("longitude") longitude: Double, @Param("latitude") latitude: Double): Iterable<Scooter>
-
-
-    @Query(
-        value = "SELECT s, ST_Distance(s.location,ST_SetSRID(ST_Point(:longitude,:latitude),:distance)) AS distance "
-                + "FROM Scooter s "
-                + "ORDER BY s.location  <-> ST_SetSRID(ST_Point(:longitude,:latitude),:distance) "
-                + "LIMIT 30",
-        nativeQuery = true
-    )
-    fun findAllByDistanceFromLocation(@Param("longitude") longitude: Double, @Param("latitude") latitude: Double, distance: Double): Iterable<Scooter>
+    fun findAvailableByLocationWithinDegree(latitude: Double, longitude: Double, degree: Double): Iterable<Scooter>
 }
-
-/*@Query("SELECT s FROM Scooter s" +
-" WHERE intersects(s.location, :area) = true" +
-" AND s.isDisabled = false AND s.isReserved = false")
-fun findAvailableWithinArea(area: Point): List<Scooter>*/
-
-//fun findByLocationWithin(location: Point, distance: Distance): Iterable<Scooter>
-
-//fun findByLocationNear(location: Point, oneKilometer: Distance): GeoResults<Scooter>
