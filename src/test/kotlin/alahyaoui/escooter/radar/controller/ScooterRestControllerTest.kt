36d939e2f26661addca3ef24e75fc777937cae93
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-24 21:40:38 +0200

    Update the tests

--- src/test/kotlin/alahyaoui/escooter/radar/controller/ScooterRestControllerTest.kt
+++ src/test/kotlin/alahyaoui/escooter/radar/controller/ScooterRestControllerTest.kt
@@ -6,6 +6,9 @@ import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.junit.jupiter.api.Test
+import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.GeometryFactory
+import org.locationtech.jts.geom.PrecisionModel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
@@ -19,16 +22,25 @@ class ScooterRestControllerTest(@Autowired val mockMvc: MockMvc) {
     private lateinit var scooterService: ScooterService
 
     @Test
-    fun `List scooters`() {
-        val scooterPony = Scooter("pony_2afe18f6934c4e8c9a1f795d4ff2c8a7", 50.854962, 4.397333, false, false, 1650036039, 39144.0)
-        val scooterLime = Scooter("e4abdccb-2353-405e-b796-47fee26945c3", 50.8157, 4.435943, false, false, 1650035878, 8004.0)
-        val scooterBird = Scooter("6f0df17f-47c4-418f-addd-24a6318dd927", 51.217471666666675, 4.427905, false, false, 1650035914, 12480.0)
+    fun `Get list of scooters by city`() {
+        val id_pony = "pony_2afe18f6934c4e8c9a1f795d4ff2c8a7"
+        val city_pony = "Brussels"
+        val company_pony = "Pony"
+        val location_pony = GeometryFactory(PrecisionModel(), 4326).createPoint(Coordinate(4.397333, 50.854962))
+        val scooterPony = Scooter(id_pony, city_pony, company_pony, location_pony, false, false, 1650036039, 39144.0)
 
-        every { scooterService.findByCity("brussels")} returns listOf(scooterPony, scooterLime)
-        mockMvc.perform(get("/api/scooter/brussels").accept(MediaType.APPLICATION_JSON))
+        val id_lime = "6f0df17f-47c4-418f-addd-24a6318dd927"
+        val city_lime = "Paris"
+        val company_lime = "Lime"
+        val location_lime = GeometryFactory(PrecisionModel(), 4326).createPoint(Coordinate(4.427905, 51.217471666666675))
+        val scooterLime = Scooter(id_lime, city_lime, company_lime, location_lime, false, false, 1650035914, 12480.0)
+
+        every { scooterService.findByCity("Paris")} returns listOf(scooterPony, scooterLime)
+        mockMvc.perform(get("/api/scooter/zone")
+            .requestAttr("city", "paris")
+            .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("\$.[0].id").value(scooterPony.bikeId))
-            .andExpect(jsonPath("\$.[1].id").value(scooterLime.bikeId))
+            .andExpect(jsonPath("\$.[0].bikeId").value(scooterPony.bikeId))
     }
 }
--- src/test/kotlin/alahyaoui/escooter/radar/repository/ScooterRepositoryTest.kt
+++ src/test/kotlin/alahyaoui/escooter/radar/repository/ScooterRepositoryTest.kt
@@ -3,6 +3,9 @@ package alahyaoui.escooter.radar.repository
 import alahyaoui.escooter.radar.entity.Scooter
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.GeometryFactory
+import org.locationtech.jts.geom.PrecisionModel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
@@ -12,7 +15,11 @@ class ScooterRepositoryTest @Autowired constructor(val entityManager: TestEntity
 
     @Test
     fun `When adding a scooter field then add the scooter to the table`() {
-        val scooter = Scooter("6f0df17f-47c4-418f-addd-24a6318dd927", 51.217471666666675, 4.427905, false, false, 1650035914, 12480.0)
+        val id = "6f0df17f-47c4-418f-addd-24a6318dd927"
+        val city = "Brussels"
+        val company = "Lime"
+        val location = GeometryFactory(PrecisionModel(), 4326).createPoint(Coordinate(4.427905, 51.217471666666675))
+        val scooter = Scooter(id, city, company, location, false, false, 1650035914, 12480.0)
         entityManager.persist(scooter)
 
         entityManager.flush()
@@ -22,7 +29,11 @@ class ScooterRepositoryTest @Autowired constructor(val entityManager: TestEntity
 
     @Test
     fun `When deleting a scooter field then delete the scooter from the table`() {
-        val scooter = Scooter("6f0df17f-47c4-418f-addd-24a6318dd927", 51.217471666666675, 4.427905, false, false, 1650035914, 12480.0)
+        val id = "6f0df17f-47c4-418f-addd-24a6318dd927"
+        val city = "Brussels"
+        val company = "Lime"
+        val location = GeometryFactory(PrecisionModel(), 4326).createPoint(Coordinate(4.427905, 51.217471666666675))
+        val scooter = Scooter(id, city, company, location, false, false, 1650035914, 12480.0)
         entityManager.persist(scooter)
 
         entityManager.flush()
@@ -33,10 +44,18 @@ class ScooterRepositoryTest @Autowired constructor(val entityManager: TestEntity
 
     @Test
     fun `When findAll then return list of Scooters`() {
-        val scooter1 = Scooter("6f0df17f-47c4-418f-addd-24a6318dd927", 51.217471666666675, 4.427905, false, false, 1650035914, 12480.0)
+        val id_scooter1 = "6f0df17f-47c4-418f-addd-24a6318dd927"
+        val city_scooter1 = "Brussels"
+        val company_scooter1 = "Lime"
+        val location_scooter1 = GeometryFactory(PrecisionModel(), 4326).createPoint(Coordinate(4.427905, 51.217471666666675))
+        val scooter1 = Scooter(id_scooter1, city_scooter1, company_scooter1, location_scooter1, false, false, 1650035914, 12480.0)
         entityManager.persist(scooter1)
 
-        val scooter2 = Scooter("pony_2afe18f6934c4e8c9a1f795d4ff2c8a7", 50.854962, 4.397333, false, false, 1650036039, 39144.0)
+        val id_scooter2 = "pony_2afe18f6934c4e8c9a1f795d4ff2c8a7"
+        val city_scooter2 = "Brussels"
+        val company_scooter2 = "Pony"
+        val location_scooter2 = GeometryFactory(PrecisionModel(), 4326).createPoint(Coordinate(4.397333, 50.854962))
+        val scooter2 = Scooter(id_scooter2, city_scooter2, company_scooter2, location_scooter2, false, false, 1650036039, 39144.0)
         entityManager.persist(scooter2)
 
         entityManager.flush()
