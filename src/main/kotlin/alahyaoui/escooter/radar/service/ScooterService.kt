d895f97ec485cbf5e6e616a60186fe89dbccc434
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-04-29 18:37:18 +0200

    Optimize fetchScootersFromProvidersTask ScooterService method

--- src/main/kotlin/alahyaoui/escooter/radar/service/ScooterService.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/service/ScooterService.kt
@@ -6,14 +6,15 @@ import alahyaoui.escooter.radar.util.getScootersFromBird
 import alahyaoui.escooter.radar.util.getScootersFromLime
 import alahyaoui.escooter.radar.util.getScootersFromPony
 import alahyaoui.escooter.radar.util.getScootersFromSpin
+import kotlinx.coroutines.*
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
 import org.springframework.core.io.ClassPathResource
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.io.BufferedReader
-import java.io.FileReader
 import java.io.InputStreamReader
+import java.time.LocalTime
 import java.util.concurrent.TimeUnit
 
 
@@ -34,27 +35,38 @@ class ScooterService(private val db: ScooterRepository) {
 
     @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
     private fun fetchScootersFromProvidersTask() {
+        println("${LocalTime.now()}: FETCHING SCOOTER FROM PROVIDERS API TASK STARTED")
         val resource = ClassPathResource("data/companies_cities.csv")
         val bufferedReader = BufferedReader(InputStreamReader(resource.inputStream))
         val csvParser = CSVParser(bufferedReader, CSVFormat.DEFAULT)
 
-        db.deleteAll()
-        for (csvRecord in csvParser) {
-            val company = csvRecord.get(0)
-            val city = csvRecord.get(1)
+        db.deleteAllInBatch()
+        println("${LocalTime.now()}: SCOOTER TABLE CLEARED")
 
-            try {
-                val scooters = when (company.lowercase()) {
-                    "pony" -> getScootersFromPony(city)
-                    "lime" -> getScootersFromLime(city)
-                    "bird" -> getScootersFromBird(city)
-                    "spin" -> getScootersFromSpin(city)
-                    else -> continue
+        val jobs = mutableListOf<Job>()
+        for (csvRecord in csvParser) {
+            jobs.add(GlobalScope.launch(Dispatchers.IO)  {
+                val company = csvRecord.get(0)
+                val city = csvRecord.get(1)
+                try {
+                    val scooters = when (company.lowercase()) {
+                        "pony" -> getScootersFromPony(city)
+                        "lime" -> getScootersFromLime(city)
+                        "bird" -> getScootersFromBird(city)
+                        "spin" -> getScootersFromSpin(city)
+                        else -> mutableListOf()
+                    }
+                    db.saveAll(scooters)
+                } catch (ex: Exception) {
+                    println("Caused by ${company} for ${city} : ${ex.message}")
                 }
-                db.saveAll(scooters)
-            } catch (ex: Exception) {
-                println("Caused by ${company} for ${city} : ${ex.message}")
+            })
+        }
+        runBlocking {
+            for (job in jobs) {
+                job.join()
             }
+            println("${LocalTime.now()}: FETCHING SCOOTER FROM PROVIDERS API TASK ENDED")
         }
     }
 }
