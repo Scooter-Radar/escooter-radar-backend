3b15620288eabf3567e9fdc28af0639c957f07d3
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-04-29 18:24:53 +0200

    Add Coroutines dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	// Coroutines Dependencies
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
 	// Hibernate Dependencies
 	implementation("org.hibernate:hibernate-spatial")
 
