dc2ae274fcd90ddf241fda90139f0a36b9d97f6b
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-05-04 12:53:20 +0200

    Update old routes

--- src/main/kotlin/alahyaoui/escooter/radar/controller/ScooterRestController.kt
+++ src/main/kotlin/alahyaoui/escooter/radar/controller/ScooterRestController.kt
@@ -14,12 +14,12 @@ class ScooterRestController(private val scooterService: ScooterService) {
         return scooterService.findByCompany(company.toSlug())
     }
 
-    @GetMapping("/zone")
+    @GetMapping("/location/zone")
     fun getScootersByZone(@RequestParam city: String): Iterable<Scooter> {
         return scooterService.findByCity(city.toSlug())
     }
 
-    @GetMapping("/location")
+    @GetMapping("/location/within")
     fun getScootersByLocationWithinDegree(@RequestParam latitude: Double, @RequestParam longitude: Double, @RequestParam degree: Double): Iterable<Scooter> {
         return scooterService.findByLocationWithinDegree(latitude, longitude, degree)
     }
